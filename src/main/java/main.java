09734e81d6f94a7914021800c4d5cd101c68efb6
Agraphie <clemens.keppler@rwth-aachen.de>
2015-05-31 15:31:32 +0200

    - quick change

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,13 +1,9 @@
 import Model.FaultTree;
 import Parser.XMLToFT;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 
 /**
  * Created by Kazako on 30.05.2015.
  */
-@Configuration
-@ComponentScan
 public class main {
 
     static XMLToFT xmlToFT = new XMLToFT();
