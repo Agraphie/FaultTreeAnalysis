292902f09425da3f11b3dd7c1e427d550a7d3fac
Agraphie <clemens.keppler@rwth-aachen.de>
2015-06-28 00:22:32 +0200

    - added forgotten file

--- src/main/java/Calculators/CalculateMCProbabilities.java
+++ src/main/java/Calculators/CalculateMCProbabilities.java
@@ -27,17 +27,18 @@ public class CalculateMCProbabilities {
 
     /**
      * Calculates the probability matrix for continous markov chains by using uniformization,
+     *
+     * @param initialProbabilities The vector of the initial probabilities.
+     * @param generatorMatrix      The generator matrix of the states.
+     * @param samplingInterval     The sampling interval, or in which intervals t changes.
+     * @param missionTime          The complete mission time till what t the probabilities should be calculated.
+     * @return The probability matrix as RealMatrix, where the row are the probabilities at time t and the columns are the states.
      * @see <a href="https://en.wikipedia.org/wiki/Uniformization_%28probability_theory%29">Uniformization (probability theory</a>
      * First, lambda gets calculated. The column count of the probability matrix is missionTime / samplingInterval + 1. +1 because of the first row,
      * which is the initial probability vector.
      * Then the transition matrix P gets calculated by adding the Identity Matrix to the generator matrix which was multiplied by 1 / lambda.
      * Next, the multiplication matrix Pi(0)*P gets calculated, where a row is the multiplication vector for the iteration n.
      * At last every row in the result is added to the result of the previous iteration to approximate the probability.
-     * @param initialProbabilities The vector of the initial probabilities.
-     * @param generatorMatrix The generator matrix of the states.
-     * @param samplingInterval The sampling interval, or in which intervals t changes.
-     * @param missionTime The complete mission time till what t the probabilities should be calculated.
-     * @return The probability matrix as RealMatrix, where the row are the probabilities at time t and the columns are the states.
      */
     public RealMatrix calculateMCProbabilities(double[] initialProbabilities, RealMatrix generatorMatrix, double samplingInterval, double missionTime) {
         if (missionTime == 0) {
@@ -50,21 +51,15 @@ public class CalculateMCProbabilities {
         int iterationPosition = 1;
         int matrixColumnCount = initialProbabilities.length;
 
-        RealMatrix multiplicationMatrix = MatrixUtils.createRealMatrix(iterations, matrixColumnCount);
         RealMatrix result = MatrixUtils.createRealMatrix(matrixEntries + 1, matrixColumnCount);
         RealVector initialProbabilitiesVector = MatrixUtils.createRealVector(initialProbabilities);
         RealMatrix identityMatrix = MatrixUtils.createRealIdentityMatrix(matrixColumnCount);
         RealMatrix transitionMatrix = identityMatrix.add(generatorMatrix.scalarMultiply(1 / lambda));
 
         RealVector tempIterationVector;
-        RealMatrix tempIterationTransitionMatrix;
 
         result.setRowVector(0, initialProbabilitiesVector);
-        for (int i = 0; i < iterations; i++) {
-            tempIterationTransitionMatrix = transitionMatrix.power(i);
-            tempIterationVector = tempIterationTransitionMatrix.preMultiply(initialProbabilitiesVector);
-            multiplicationMatrix.setRowVector(i, tempIterationVector);
-        }
+        RealMatrix multiplicationMatrix = calculateMultiplicationMatrix(iterations, initialProbabilitiesVector, transitionMatrix);
 
         for (double t = samplingInterval; t <= missionTime; t += samplingInterval) {
             tempIterationVector = result.getRowVector(iterationPosition);
@@ -80,13 +75,26 @@ public class CalculateMCProbabilities {
             result.setRowVector(iterationPosition, tempIterationVector);
             iterationPosition++;
         }
-        System.out.println(result.toString());
 
         return result;
     }
 
+    private RealMatrix calculateMultiplicationMatrix(int iterations, RealVector initialProbabilitiesVector, RealMatrix transitionMatrix) {
+        RealMatrix tempIterationTransitionMatrix;
+        RealVector tempIterationVector;
+        RealMatrix multiplicationMatrix = MatrixUtils.createRealMatrix(iterations, transitionMatrix.getColumnDimension());
+        for (int i = 0; i < iterations; i++) {
+            tempIterationTransitionMatrix = transitionMatrix.power(i);
+            tempIterationVector = tempIterationTransitionMatrix.preMultiply(initialProbabilitiesVector);
+            multiplicationMatrix.setRowVector(i, tempIterationVector);
+        }
+
+        return multiplicationMatrix;
+    }
+
     /**
      * Find lambda by searching the maximum on the diagonal of the generator matrix.
+     *
      * @param generatorMatrix The original generator matrix.
      * @return The maximum on the diagonal of the generator matrix.
      */
@@ -102,64 +110,10 @@ public class CalculateMCProbabilities {
         return lambda;
     }
 
-    public void foo(BDD bdd, RealMatrix realMatrix) {
-        meh = realMatrix;
-        HashSet<BDD> mc1 = new HashSet<>();
-        HashSet<BDD> mc2 = new HashSet<>();
-        mc1.add(bdd.getFactory().ithVar(0));
-        mc1.add(bdd.getFactory().ithVar(1));
-        mc1.add(bdd.getFactory().ithVar(2));
-
-        mc2.add(bdd.getFactory().ithVar(3));
-        mc2.add(bdd.getFactory().ithVar(4));
-        mc2.add(bdd.getFactory().ithVar(5));
-
-        mc.add(mc1);
-        mc.add(mc2);
-
-        System.out.println(prob(bdd) + " prob");
-    }
-
-    public double prob(BDD bdd) {
-        if (bdd.isOne()) {
-            return 1;
-        } else if (bdd.isZero()) {
-            return 0;
-        } else if (visited.containsKey(bdd.hashCode())) {
-            System.out.println("visiiited");
-            return visited.get(bdd.hashCode());
-        } else {
-            double probability;
-            double probabilityLow = prob(bdd.low());
-            boolean found = false;
-            for (HashSet<BDD> hashSet : mc) {
-                if (hashSet.contains(bdd) && hashSet.contains(bdd.low())) {
-                    found = true;
-                }
-            }
-            if (found) {
-                probability = probabilityLow + meh.getEntry(1, bdd.var()) * (prob(bdd.high()) - probabilityLow);
-            } else {
-                probability = probabilityLow + meh.getEntry(1, bdd.var()) * (prob(bdd.high()) - prob(bdd.low().low()));
-            }
-
-            visited.put(bdd.hashCode(), probability);
-
-            return probability;
-        }
-    }
-
-    public boolean aDependentB(BDD a, BDD b) {
-
-        return true;
-    }
-
     public RealMatrix calculateTopEvent(BDD bdd, RealMatrix failureMatrix, HashMap<Integer, Integer> markovChains) {
         List list = (List) bdd.allsat();
         RealMatrix failureRateWithTopEventFailure = MatrixUtils.createRealMatrix(failureMatrix.getRowDimension(), failureMatrix.getColumnDimension() + 1);
         failureRateWithTopEventFailure.setSubMatrix(failureMatrix.getData(), 0, 0);
-        double tempProb;
-        double calculatedProb;
         for (Object satObject : list) {
             byte[] sat = (byte[]) satObject;
             //scan for independence
@@ -169,24 +123,23 @@ public class CalculateMCProbabilities {
         return failureRateWithTopEventFailure;
     }
 
+    /**
+     * Method scans first if the demanded states are independent. It iterates over every solution obtained by calling allSat(). Every solution will provide an
+     * array. Over every such array the method iterates and compares every entry (variable) to every other variable. If the compared variables are set to 1 and
+     * are in the same markov chains, the probability for that solution is set to 0.
+     * If a variable is set to 1, it gets multiplied with the temp probability. If it is set to 0, 1 - the probability gets multiplied.
+     *
+     * @param failureMatrix                  The matrix containing all probabilities for the failures.
+     * @param markovChains                   The markov chains, stating which variables are in one chain.
+     * @param failureRateWithTopEventFailure The resulting failure matrix with the top event probability.
+     * @param sat                            The current sat solution.
+     */
     private void scanForIndependenceAndCalculateProbabilities(RealMatrix failureMatrix, HashMap<Integer,
             Integer> markovChains, RealMatrix failureRateWithTopEventFailure, byte[] sat) {
         double tempProb;
         double calculatedProb;
-        int m;
-        int u;
-
-        //are the states independent?
-        for (int j = 0; j < sat.length; j++) {
-            m = sat[j];
-            for (int n = j; n < sat.length; n++) {
-                u = sat[n];
-                if (j != n && m == 1 && u == 1) {
-                    if (markovChains.get(j) == markovChains.get(n)) {
-                        return;
-                    }
-                }
-            }
+        if (scanForIndependence(markovChains, sat)) {
+            return;
         }
 
         //they really are independent
@@ -202,5 +155,22 @@ public class CalculateMCProbabilities {
             }
             failureRateWithTopEventFailure.setEntry(j, failureMatrix.getColumnDimension(), tempProb + calculatedProb);
         }
+    }
+
+    private boolean scanForIndependence(HashMap<Integer, Integer> markovChains, byte[] sat) {
+        int m;
+        int u;//are the states independent?
+        for (int j = 0; j < sat.length; j++) {
+            m = sat[j];
+            for (int n = j; n < sat.length; n++) {
+                u = sat[n];
+                if (j != n && m == 1 && u == 1) {
+                    if (markovChains.get(j) == markovChains.get(n)) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
     }
 }
